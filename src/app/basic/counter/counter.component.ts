d970b98b37abfc3755a8ae3c0ff4ee2849522e49
RogerCastillo877 <roger.castillo877@gmail.com>
2023-08-10 17:12:21 -0500

    feature: adding first test

--- src/app/basic/counter/counter.component.ts
+++ src/app/basic/counter/counter.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html'
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
 
+  counter: number = 10;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  increaseBy(value: number) {
+    this.counter += value
+  }
 }
