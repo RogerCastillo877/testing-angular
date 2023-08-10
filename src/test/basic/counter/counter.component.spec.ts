b1fda95fe2c8488f76b59d1831de5dddbb335174
RogerCastillo877 <roger.castillo877@gmail.com>
2023-08-10 17:13:21 -0500

    feature: adding data base and json server config

---
+++ src/test/basic/counter/counter.component.spec.ts
@@ -0,0 +1,46 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CounterComponent } from '../../../../src/app/basic/counter/counter.component';
+
+describe('CounterComponent', () => {
+  let component: CounterComponent;
+  let fixture: ComponentFixture<CounterComponent>;
+  let compiled: HTMLElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CounterComponent]
+    });
+    fixture = TestBed.createComponent(CounterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    compiled = fixture.nativeElement;
+  });
+
+  test('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  test('should be equal to snapshot', () => {
+    expect(compiled).toMatchSnapshot();
+  });
+
+  test('should be increaseBy based on the argument', () => {
+    component.increaseBy(5);
+    expect(component.counter).toBe(15);
+  })
+
+  test('should increment and decrement in 1, when click buttons', () => {
+    const buttons = compiled.querySelectorAll('button');
+    buttons[0].click();
+    expect(component.counter).toBe(11)
+    buttons[1].click();
+    expect(component.counter).toBe(10)
+  })
+
+  test('should refresh h1 tag, when counter change', () => {
+    component.increaseBy(10);
+    fixture.detectChanges();
+    const h1 = compiled.querySelector('h2')
+    expect(h1?.textContent).toContain('20');
+  })
+});
